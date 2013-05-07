01da97115a498b038d23ecabd1a4387f0631a850
Eddy Beaupre <eddy@beaupre.biz>
2013-05-07 19:24:18 -0400

    - Added function for the buildroot process.

--- lib/armbian.sh
+++ lib/armbian.sh
@@ -1,24 +1,13 @@
-# usage bootStrap <ARCH> <SUITE> [<MIRROR>]
-function bootStrap {
-  if [ $# -eq 2 ]; then
-    printStatus "bootStrap" "Running debootstrap --foreign --arch ${1} ${2}, target is ${BUILD_MNT_ROOT}"
-    debootstrap --foreign --arch ${1} ${2} ${BUILD_MNT_ROOT}/ >> ${BUILD_LOG_FILE} 2>&1
-    checkStatus "debootstrap failed with status ${?}"
-  elif [ $# -eq 3 ]; then
-    printStatus "bootStrap" "Running debootstrap --foreign --arch ${1} ${2} using mirror ${3}, target is ${BUILD_MNT_ROOT}"
-    debootstrap --foreign --arch ${1} ${2} ${BUILD_MNT_ROOT}/ ${3} >> ${BUILD_LOG_FILE} 2>&1
-    checkStatus "debootstrap failed with status ${?}"
-  else
-    checkStatus "bootStrap need 2 or 3 arguments."
-  fi
-  
-  printStatus "bootStrap" "Running debootstrap --second-stage"
-  LC_ALL=${BUILD_LANG} LANGUAGE=${BUILD_LANG} LANG=${BUILD_LANG} chroot ${BUILD_MNT_ROOT}/ /debootstrap/debootstrap --second-stage >> ${BUILD_LOG_FILE} 2>&1
-  checkStatus "debootstrap --second-stage failed with status ${?}"
+# Usage installQEMU <ARCH>
+function installQEMU {
+  printStatus installQEMU "Installing QEMU User Emulator (${1})"  
+  cp /usr/bin/qemu-${1}-static ${BUILD_MNT_ROOT}/usr/bin
+}
 
-  printStatus "bootStrap" "Running dpkg --configure -a"
-  LC_ALL=${BUILD_LANG} LANGUAGE=${BUILD_LANG} LANG=${BUILD_LANG} chroot ${BUILD_MNT_ROOT}/ dpkg --configure -a >> ${BUILD_LOG_FILE} 2>&1
-  checkStatus "dpkg --configure failed with status ${?}"
+# Usage removeQEMU <ARCH>
+function removeQEMU {
+  printStatus removeQEMU "Removing QEMU User Emulator (${1})"  
+  rm -f ${BUILD_MNT_ROOT}/usr/bin
 }
 
 # Usage : disableServices
@@ -34,7 +23,33 @@ EOF
 # Usage : enableServices
 function enableServices {
   printStatus "disableServices" "Enabling services startup in ${BUILD_MNT_ROOT}"
-  rm ${BUILD_MNT_ROOT}/usr/sbin/policy-rc.d
+  rm -f ${BUILD_MNT_ROOT}/usr/sbin/policy-rc.d
+}
+
+# usage bootStrap <ARCH> <EABI> <SUITE> [<MIRROR>]
+function bootStrap {
+  if [ $# -eq 3 ]; then
+    printStatus "bootStrap" "Running debootstrap --foreign --arch ${1}${2} ${3}, target is ${BUILD_MNT_ROOT}"
+    debootstrap --foreign --arch ${1}${2} ${3} ${BUILD_MNT_ROOT}/ >> ${BUILD_LOG_FILE} 2>&1
+    checkStatus "debootstrap failed with status ${?}"
+  elif [ $# -eq 4 ]; then
+    printStatus "bootStrap" "Running debootstrap --foreign --arch ${1}${2} ${3} using mirror ${4}, target is ${BUILD_MNT_ROOT}"
+    debootstrap --foreign --arch ${1}${2} ${3} ${BUILD_MNT_ROOT}/ ${4} >> ${BUILD_LOG_FILE} 2>&1
+    checkStatus "debootstrap failed with status ${?}"
+  else
+    checkStatus "bootStrap need 2 or 3 arguments."
+  fi
+  
+  installQEMU ${1}
+  disableServices
+
+  printStatus "bootStrap" "Running debootstrap --second-stage"
+  LC_ALL=${BUILD_LANG} LANGUAGE=${BUILD_LANG} LANG=${BUILD_LANG} chroot ${BUILD_MNT_ROOT}/ /debootstrap/debootstrap --second-stage >> ${BUILD_LOG_FILE} 2>&1
+  checkStatus "debootstrap --second-stage failed with status ${?}"
+
+  printStatus "bootStrap" "Running dpkg --configure -a"
+  LC_ALL=${BUILD_LANG} LANGUAGE=${BUILD_LANG} LANG=${BUILD_LANG} chroot ${BUILD_MNT_ROOT}/ dpkg --configure -a >> ${BUILD_LOG_FILE} 2>&1
+  checkStatus "dpkg --configure failed with status ${?}"
 }
 
 # Usage : setHostName <HOSTNAME>
@@ -48,7 +63,7 @@ EOF
 # Usage : clearSourcesList
 function clearSourcesList {
   printStatus "clearSourcesList" "Removing current sources list"
-  rm ${BUILD_MNT_ROOT}/etc/apt/sources.list
+  rm -f ${BUILD_MNT_ROOT}/etc/apt/sources.list
   touch ${BUILD_MNT_ROOT}/etc/apt/sources.list
 }
 
@@ -92,19 +107,80 @@ EOF
 
 # Usage addInitTab <ID> <RUNLEVELS> <DEVICE> <SPEED> <TYPE>
 function addInitTab {
+  printStatus "addInitTab" "Configuring terminal ${1} for runlevels ${2} on device ${3} at ${4}bps (${5})"
   printf "%s:%s:respawn:/sbin/getty -L %s %s %s\n" ${1} ${2} ${3} ${4} ${5} >> ${BUILD_MNT_ROOT}/etc/inittab
 }
 
 # Usage addFStab <file system> <mount point> <type> <options> <dump> <pass>
 function addFStab {
+  printStatus "addFStab" "Device ${1} will be mount as ${2}"
   if [ ! -e "${BUILD_MNT_ROOT}/etc/fstab" ]; then
     printf "#%- 15s% -15s%- 10s%- 15s%- 10s%- 10s\n" "<file system>" "<mount point>" "<type>" "<options>" "<dump>" "<pass>" > ${BUILD_MNT_ROOT}/etc/fstab
   fi
   printf "%- 15s% -15s%- 10s%- 15s%- 10s%- 10s\n" >> ${BUILD_MNT_ROOT}/etc/fstab
 }
 
-# usage addModules <KERNEL MODULE>
+# usage addModules <KERNEL MODULE> [<COMMENT>]
 function addModule {
-  printf "%s\n" {$1} >> ${BUILD_MNT_ROOT}/etc/modules
+
+  printStatus "addModule" "Configuring kernel module ${1}"
+  if [ ! -z "${2}" ]; then
+    printf "\n# %s\n", ${2} >> ${BUILD_MNT_ROOT}/etc/modules
+  fi
+  
+  printf "%s\n" ${1} >> ${BUILD_MNT_ROOT}/etc/modules
 }
 
+# Usage : addIface <INTERFACE> <dhcp|static> [<address> <netmask> <gateway>]
+function addIface {
+  printStatus "addIface" "Configuring interface ${1}"
+  printf "auto %s\n" ${1} >> ${BUILD_MNT_ROOT}/etc/network/interfaces
+  printf "allow-hotplug %s\n\n" ${1} >> ${BUILD_MNT_ROOT}/etc/network/interfaces
+  printf "iface %s inet %s\n" ${1} ${2} >> ${BUILD_MNT_ROOT}/etc/network/interfaces
+  if [ "${2}" != "dhcp" ]; then
+    printStatus "addIface" "IP address : ${3}/${4}, default gateway ${5}"
+    printf "  address %s\n" ${3} >> ${BUILD_MNT_ROOT}/etc/network/interfaces
+    printf "  netmask %s\n" ${4} >> ${BUILD_MNT_ROOT}/etc/network/interfaces
+    printf "  gateway %s\n\n" ${5} >> ${BUILD_MNT_ROOT}/etc/network/interfaces
+  else
+    printStatus "addIface" "IP address : DHCP"
+    printf "\n" >> ${BUILD_MNT_ROOT}/etc/network/interfaces
+  fi
+}
+
+# Usage : initResolvConf
+function initResolvConf {
+  printStatus "initResolvConf" "Initializing ${BUILD_MNT_ROOT}/etc/resolv.conf"
+  rm -f ${BUILD_MNT_ROOT}/etc/resolv.conf
+  touch ${BUILD_MNT_ROOT}/etc/resolv.conf
+}
+
+# Usage : addSearchDomain <DOMAIN>
+function addSearchDomain {
+  printf "addSearchDomain" "Configuring search domain to ${1}"
+  printf "search %s\n" ${1} >> ${BUILD_MNT_ROOT}/etc/resolv.conf
+}
+
+# Usage : addNameServer <NS1> [<NS2> ... ]
+function addNameServer {
+  for i in ${@}; do
+    printStatus "addNameServer" "Configuring dns server ${i}"
+    printf "nameserver %s\n" ${i} >> ${BUILD_MNT_ROOT}/etc/resolv.conf
+  done
+}
+
+
+# Usage : bootClean <ARCH>
+function bootClean {
+  printStatus "bootClean" "Running aptitude update"
+  LC_ALL=${BUILD_LANG} LANGUAGE=${BUILD_LANG} LANG=${BUILD_LANG} chroot ${BUILD_MNT_ROOT}/ aptitude --quiet -y update >> ${BUILD_LOG_FILE} 2>&1
+  
+  printStatus "bootClean" "Running aptitude clean"
+  LC_ALL=${BUILD_LANG} LANGUAGE=${BUILD_LANG} LANG=${BUILD_LANG} chroot ${BUILD_MNT_ROOT}/ aptitude --quiet -y clean  >> ${BUILD_LOG_FILE} 2>&1
+  
+  printStatus "bootClean" "Running apt-get clean"
+  LC_ALL=${BUILD_LANG} LANGUAGE=${BUILD_LANG} LANG=${BUILD_LANG} chroot ${BUILD_MNT_ROOT}/ apt-get --quiet clean >> ${BUILD_LOG_FILE} 2>&1
+  
+  removeQEMU ${1}
+  enableServices
+}
