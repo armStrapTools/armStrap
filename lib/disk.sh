8900b2abd5614dd7db889e8e567b0733c8f6c731
Eddy Beaupre <eddy@beaupre.biz>
2013-05-21 18:42:27 -0400

    - Bug Fix

--- lib/disk.sh
+++ lib/disk.sh
@@ -1,6 +1,6 @@
 
-# Usage: mkImage <FILE> <SIZE IN MB>
-function mkImage {
+# Usage: makeImg <FILE> <SIZE IN MB>
+function makeImg {
 
   printStatus "mkImage" "Creating image ${1}, size ${2}MB"
   
@@ -12,6 +12,7 @@ function mkImage {
 
   dd if=/dev/zero of=${1} bs=1M count=${2} >> ${ARMSTRAP_LOG_FILE} 2>&1
   checkStatus "dd exit with status $?"
+  partSync
 }
 
 # Usage partDevice <DEVICE> <SIZE:FS> [<SIZE:FS> ...]
@@ -26,7 +27,7 @@ function partDevice {
     local TMP_ARR=(${i//:/ })
     if [ "${TMP_ARR[0]}" -gt "0" ]; then
       local TMP_SIZE=$(($TMP_OFF + ${TMP_ARR[0]}))
-      printStatus "partDevice" "Creating a ${TMP_SIZE}Mb partition (${TMP_ARR[1]})" 
+      printStatus "partDevice" "Creating a ${TMP_ARR[0]}Mb partition (${TMP_ARR[1]})" 
       parted ${TMP_DEV} --script -- mkpart primary ${TMP_ARR[1]} ${TMP_OFF} ${TMP_SIZE} >> ${ARMSTRAP_LOG_FILE} 2>&1
       checkStatus "parted exit with status $?"
       TMP_OFF=$(($TMP_SIZE + 1))
@@ -36,6 +37,7 @@ function partDevice {
       checkStatus "parted exit with status $?"
     fi
   done
+  partSync
 }
 
 # Usage loopImg <FILE>
@@ -43,6 +45,7 @@ function loopImg {
   printStatus "loopImg" "Attaching ${1} to loop device"
   ARMSTRAP_DEVICE_LOOP=($(losetup -f --show "${1}"))
   checkStatus "losetup exit with status $?"
+  partSync
 }
 
 # Usage uloopImg
@@ -50,21 +53,24 @@ function uloopImg {
   printStatus "uloopImg" "Detaching ${ARMSTRAP_DEVICE_LOOP} from loop device"
   losetup -d ${ARMSTRAP_DEVICE_LOOP} >> ${ARMSTRAP_LOG_FILE} 2>&1
   checkStatus "losetup exit with status $?"
+  partSync
 }
 
 # Usage mapImg <FILE>
-# XXX NEED TO SET A VARIABLE TO WORK!!!
 function mapImg {
+  local TMP_MAP
   printStatus "mapImg" "Mapping ${1} to loop device"
   while read i; do
     x=($i)
-    if [ -z "${ARMSTRAP_DEVICE_MAPS}" ]; then
-      ARMSTRAP_DEVICE_MAPS="/dev/mapper/${x[2]}"
+    if [ -z "${TMP_MAP}" ]; then
+      TMP_MAP="/dev/mapper/${x[2]}"
     else
-      ARMSTRAP_DEVICE_MAPS="${ARMSTRAP_DEVICE_MAPS} /dev/mapper/${x[2]}"
+      TMP_MAP="${TMP_MAP} /dev/mapper/${x[2]}"
     fi
   done <<< "`kpartx -avs ${1}`"
   checkStatus "kpartx exit with status $?"
+  ARMSTRAP_DEVICE_MAPS=(${TMP_MAP})
+  partSync
 }
 
 # Usage umapImg <FILE>
@@ -72,16 +78,18 @@ function umapImg {
   printStatus "umapImg" "UnMapping ${1} from loop device"
   kpartx -d ${1} >> ${ARMSTRAP_LOG_FILE} 2>&1
   checkStatus "kpartx exit with status $?"
+  partSync
 }
 
 # Usage formatPartitions <DEVICE:FS> [<DEVICE:FS> ...]
 function formatPartitions {
   for i in "$@"; do
     local TMP_ARR=(${i//:/ })
-    printStatus "fmtParts" "Formatting ${0} (${TMP_ARR[1]})"
+    printStatus "fmtParts" "Formatting ${1} (${TMP_ARR[1]})"
     mkfs.${TMP_ARR[1]} ${TMP_ARR[0]} >> ${ARMSTRAP_LOG_FILE} 2>&1
     checkStatus "mkfs.${TMP_ARR[1]} exit with status $?"
   done
+  partSync
 }
 
 # Usage mountPartitions <DEVICE:MOUNTPOINT> [<DEVICE:MOUNTPOINT> ...]
@@ -91,6 +99,7 @@ function mountPartitions {
     checkDirectory "${TMP_ARR[1]}"
     mount ${TMP_ARR[0]} ${TMP_ARR[1]} >> ${ARMSTRAP_LOG_FILE} 2>&1
   done
+  partSync
 }
 
 # Usage umountPartitions <MOUNTPOINT> [<MOUNTPOINT> ...]
@@ -98,25 +107,5 @@ function umountPartitions {
   for i in "$@"; do
     umount ${i} >> ${ARMSTRAP_LOG_FILE} 2>&1
   done
+  partSync
 }
-
-#createImg ${IMG_FILE} 1024
-
-#tmp=($(loopImg "${IMG_FILE}"))
-#partDevice "${tmp}" "16:fat32" "512:ext4" "-1:ext4"
-#uloopImg "${tmp}"
-
-#tmp=($(mapImg "${IMG_FILE}"))
-#for i in "${tmp[@]}"; do
-#    echo " Map Device : $i"
-#done
-
-#formatPartitions "${tmp[0]}:vfat" "${tmp[1]}:ext4" "${tmp[2]}:ext4" 
-#
-#mountPartitions "${tmp[2]}:./root" "${tmp[1]}:./root/home" "${tmp[0]}:./root/boot"
-#
-#df
-#
-#umountPartitions "./root/boot" "./root/home" "./root"
-#
-#umapImg ${IMG_FILE}
