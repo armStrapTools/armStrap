64380704aa0e01945c6988b703c937e2abe027a5
Eddy Beaupre <eddy@beaupre.biz>
2013-07-15 00:16:05 -0400

    Reset default configuration to CubieBoard.

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 # Build configuration
 #
 # Set this to the name of the board you want to build
-ARMSTRAP_CONFIG="CubieBoard2K34"
+ARMSTRAP_CONFIG="CubieBoard"
 
 ##############################################################################
 # Basic configuration
