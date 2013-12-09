a61f3bc32b9f31716e3011da2d3a548420cdc4c6
EddyBeaupre <eddy@beaupre.biz>
2013-12-08 22:58:09 -0500

    Sync.

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 # Build configuration
 #
 # Set this to the name of the board you want to build
-ARMSTRAP_CONFIG="CubieBoard"
+ARMSTRAP_CONFIG="CubieTruck"
 
 ##############################################################################
 # Basic configuration
